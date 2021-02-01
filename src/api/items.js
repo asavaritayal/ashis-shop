8e396d1fa4911bd00ef08a327b9d6845cacf5b42
Asavari Tayal <asavaritayal@Asavaris-MacBook-Pro.local>
2021-01-31 22:33:59 -0800

    products

--- src/api/items.js
+++ src/api/items.js
@@ -1,20 +1,20 @@
 export default [
-    {
-      id: 1,
-      title: 'Bike',
-      price: 50,
-      category: 'Sports',
-    },
-    {
-      id: 2,
-      title: 'Tennis Racquet',
-      price: 25,
-      category: 'Sports',
-    },
-    {
-      id: 3,
-      title: 'Football',
-      price: 30,
-      category: 'Sports',
-    },
-  ];
+  {
+    id: 1,
+    title: 'Bike',
+    price: 50,
+    category: 'Sports',
+  },
+  {
+    id: 2,
+    title: 'Tennis Racquet',
+    price: 25,
+    category: 'Sports',
+  },
+  {
+    id: 3,
+    title: 'Football',
+    price: 30,
+    category: 'Sports',
+  },
+];
